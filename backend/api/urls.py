265c6078ea3b35c3887fd9bcbf812b239048bb0b
67val <skeppylatenci@gmail.com>
2026-04-13 16:46:39 +0800

    Changed Admin and user key

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from api.controller import user
+import api.views as view
 
-
+from django.contrib import admin
 urlpatterns = [
     path('post-user/', user.create_user, name='user'),
     path('signed-in/', user.fetch_user_data, name='fetch_user_data'),
+    path('create-api-key/', view.view_user, name='createapikey'),
+    path('admin/', admin.site.urls, name="admin")
 ]
 
     # path('put-user/<int:idNum>/', views.put_user, name='put'),
---
+++ backend/api/views.py
@@ -0,0 +1,50 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from api.serializer import BaseUserSerializer
+
+from api.model.user.baseuser import BaseUser
+from api.model.leveledapikey import LeveledApiKey
+from api.permission import AdminPermissions
+from rest_framework_api_key.permissions import BaseHasAPIKey
+
+from random import randint
+
+# k2Fnz6QF.FX4vHYnZBKjgnL8r9NPCpQFGs3uCqrIY - Admin
+# iNgApKUo.prslIn0uQgJIDbabJMxD5RxCZnNirAKl - User
+
+
+class HasLeveledAPIKey(BaseHasAPIKey):
+    model = LeveledApiKey
+
+@api_view(['GET', 'POST'])
+def view_user(request):
+    serializer = BaseUserSerializer(data=request.data)
+
+    numOfId = BaseUser.objects.values_list('idNum', flat=True)
+    idRand = randint(10000, 99999)
+    while idRand in numOfId:
+        idRand = randint(10000, 99999)
+    
+
+    if serializer.is_valid():
+        student = serializer.save(idNum=idRand)
+
+        api_key_name = f'Key for joshheraldabelardo'
+        api_key_instance, key_token = LeveledApiKey.objects.create_key(
+            name=api_key_name,
+            user=student,
+            level='ADMIN'
+        )
+
+        return Response({
+            'success': True,
+            'data': serializer.data,
+            'message': 'Save this and don\'t show it to ANYONE',
+            'api_key': key_token
+        }, status = status.HTTP_201_CREATED)
+    else:
+        return Response({
+            'success': False,
+            'data': serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
